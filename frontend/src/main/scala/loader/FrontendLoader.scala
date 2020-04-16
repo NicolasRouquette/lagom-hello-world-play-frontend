ec1038c7c747a13f15373b7d13474ce2cfa43320
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2020-04-16 15:44:23 -0700

    Thanks @ignasi35 for finding the root cause of the misbehavior!

--- frontend/src/main/scala/loader/FrontendLoader.scala
+++ frontend/src/main/scala/loader/FrontendLoader.scala
@@ -3,15 +3,12 @@ package loader
 import com.lightbend.lagom.scaladsl.akka.discovery.AkkaDiscoveryComponents
 import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
 import play.api.ApplicationLoader.Context
-import play.api.{Application, ApplicationLoader, Environment, LoggerConfigurator, Mode}
-import play.filters.HttpFiltersComponents
+import play.api.{Application, ApplicationLoader, Mode}
 
 class FrontendLoader extends ApplicationLoader {
 
   override def load(context: Context): Application = {
-    val environment = context.environment
-    loadCustomLoggerConfiguration(environment)
-    environment.mode match {
+    context.environment.mode match {
       case Mode.Dev => loadDevMode(context)
       case _        => loadInternal(context)
     }
@@ -23,12 +20,4 @@ class FrontendLoader extends ApplicationLoader {
   def loadDevMode(context: Context): Application =
     (new Frontend(context, Seq.empty) with LagomDevModeComponents).application
 
-  /**
-    * Fix for https://github.com/lagom/lagom/issues/534
-    */
-  private def loadCustomLoggerConfiguration(environment: Environment): Unit = {
-    LoggerConfigurator(environment.classLoader).foreach {
-      _.configure(environment)
-    }
-  }
 }
